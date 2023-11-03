b9874d6f5403e345e1b5ba825e1756c5f32017c1
lilmicke <lilmickey0010@gmail.com>
2023-11-03 11:41:37 +0300

    first commit

---
+++ app/src/main/java/com/example/catapp/ui/theme/Screens/JM.kt
@@ -0,0 +1,12 @@
+package com.example.catapp.ui.theme.Screens
+
+import androidx.compose.material3.Text
+
+class JM {
+
+
+
+
+//    println("Your BMI is %.2f, which is categorized as $category.".format(bmi))
+
+}
